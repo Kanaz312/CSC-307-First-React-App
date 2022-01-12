19741c8e19aabe49a98aefa742542e31484c5185
Kanaz312 <63126907+Kanaz312@users.noreply.github.com>
2022-01-12 13:20:33 -0800

    Added Delete on Backend

--- src/MyApp.js
+++ src/MyApp.js
@@ -7,16 +7,18 @@ function MyApp() {
   const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index
+    makeDelCall(characters[index].id).then(resp => {
+      const updated = characters.filter((character, i) => {
+        return i !== index
+      });
+      setCharacters(updated);
     });
-    setCharacters(updated);
   }
 
   function updateList(person){
     makePostCall(person).then(result => {
-      if (result && result.status == 200)
-        setCharacters([...characters, person]);
+      if (result && result.status == 201)
+        setCharacters([...characters, result.data]);
     });
   }
 
@@ -33,6 +35,16 @@ function MyApp() {
   async function makePostCall(person) {
     try {
       const resp = await axios.post('http://localhost:5000/users', person);
+      return resp;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async function makeDelCall(id){
+    try {
+      const resp = await axios.delete(`http://localhost:5000/users/${id}`);
       return resp;
     } catch (error) {
       console.log(error);
