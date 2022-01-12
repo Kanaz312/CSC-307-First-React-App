ce5e6e90ad621ccf40e5506d30c8974298f0dabe
Kanaz312 <63126907+Kanaz312@users.noreply.github.com>
2022-01-12 13:04:38 -0800

    Added Post to Backend

--- src/MyApp.js
+++ src/MyApp.js
@@ -14,14 +14,27 @@ function MyApp() {
   }
 
   function updateList(person){
-    setCharacters([...characters, person]);
+    makePostCall(person).then(result => {
+      if (result && result.status == 200)
+        setCharacters([...characters, person]);
+    });
   }
 
   async function fetchAll() {
-    try{
+    try {
       const resp = await axios.get('http://localhost:5000/users');
       return resp.data.users_list;
-    } catch (error){
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async function makePostCall(person) {
+    try {
+      const resp = await axios.post('http://localhost:5000/users', person);
+      return resp;
+    } catch (error) {
       console.log(error);
       return false;
     }
@@ -29,8 +42,10 @@ function MyApp() {
 
   useEffect(() => {
     fetchAll().then(result => {
-      if (result)
+      if (result){
         setCharacters(result);
+        console.log("Set Chars");
+      }
     });
   }, []);
 
